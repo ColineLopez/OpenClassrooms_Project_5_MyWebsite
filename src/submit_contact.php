4f6bce97d720407ffd4c56d7ebc59720b56f764f
Coline Lopez <coline.llopez@gmail.com>
2023-08-04 16:46:41 +0200

    contact form working

---
+++ src/submit_contact.php
@@ -0,0 +1,40 @@
+<?php 
+    require_once "config.php";
+
+    if(isset($_POST['lastname']) && isset($_POST['firstname']) && isset($_POST['email'])  && isset($_POST['message'])){
+
+        $lastname = htmlspecialchars($_POST['lastname']);
+        $firstname = htmlspecialchars($_POST['firstname']);
+        $email = htmlspecialchars($_POST['email']);
+        $message = htmlspecialchars($_POST['message']);
+
+        $check = $database->prepare('SELECT lastname, firstname, email, message FROM contact WHERE email = ?');
+
+        $check->execute(array($email));
+        $data = $check->fetch();
+        $row = $check->rowCount();
+
+
+        if(strlen($lastname)<=25)
+        {
+            if(strlen($firstname)<=25)
+            {
+                if(strlen($email)<=100)
+                {
+                    if(filter_var($email, FILTER_VALIDATE_EMAIL))
+                    {
+                        $insert = $database->prepare('INSERT INTO contact(lastname, firstname, email, message) VALUES(:lastname, :firstname, :email, :message)');
+                            $insert->execute(array(
+                                'lastname'     => $lastname,
+                                'firstname'  => $firstname,
+                                'email'   => $email,
+                                'message' => $message));
+                            header('Location:../index.php?reg_err=success');
+                    }else header('Location:../index.php#contact?reg_err=email');
+                }else header('Location:../index.php#contact?reg_err=email_length');
+            }else header('Location:../index.php#contact?reg_err=firstname_length');
+        }else header('Location:../index.php#contact?reg_err=lastname_length');
+    }
+
+
+?>
